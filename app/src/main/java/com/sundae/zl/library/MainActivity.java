4906a188b78dbfd6ace3ccd15d7292fa8adb921c
周龙 <zl_sundae@qq.com>
2017-10-11 13:45:37 +0800

    init

---
+++ app/src/main/java/com/sundae/zl/library/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sundae.zl.library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
